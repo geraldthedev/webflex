549e6cfa0551b6ed76de3ad5b99367e9eb406a2e
Gerald Bryant <hakeem1106@gmail.com>
2019-03-13 18:39:12 +0000

    page setup

---
+++ components/PostIt.js
@@ -0,0 +1,18 @@
+Impr
+
+class PostIt extends React.Component {
+beginAgain =() =>{
+
+}
+
+     render(){
+         return(
+             <div className="Getter">
+             <button onClick={this.beginAgain} >Start</button>
+
+             </div>
+         )
+
+
+     }
+ }
