2598af16a9870465226bd93ccfe409ec4b2684cd
Gerald Bryant <hakeem1106@gmail.com>
2019-03-17 21:01:57 +0000

    looking good

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 
 export default class CountUp extends React.Component{
+
     render(){
         return(
             <div className="playholder">
-            
+            <h2>{this.props.name}</h2>
+            <h2>{this.props.github}</h2>
+            <h2>{this.props.phone}</h2>
+            <h2>{this.props.email}</h2>
             </div>
         )
+
     }
+
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,12 @@ import '../index.css'
 function Main(props){
     return(
         <div>
-        <CountUp />
+        <CountUp
+        name="Gerald Bryant"
+        github="https://github.com/hakeem1106"
+        phone="555-555-5555"
+        email="testprops@fakemail.com"
+         />
         </div>
     )
 }
